759d0e1fc345942ffc1a7068c0ad891efb0d3f8d
antfarmar <markfarrugiamail@gmail.com>
2015-11-25 18:32:03 -0500

    Stablized algorithm.

--- Assets/Scripts/ScreenWrapper.cs
+++ Assets/Scripts/ScreenWrapper.cs
@@ -1,54 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 // Screenwrap a gameObject OnBecameInvisible() message.
 // Hacky, see comments below. Affected by Scene view too.
 public class ScreenWrapper : MonoBehaviour
 {
-    bool isWrappingX = false;
-    bool isWrappingY = false;
+
+    bool IsVisible = true;
+
+
+    void Update()
+    {
+        if(IsVisible)
+            return;
+        else
+        {
+            ScreenWrap();
+            IsVisible = true;
+        }
+    }
+
 
     // Note that object is considered visible when it needs to be rendered in the scene.
     // It might not be actually visible by any camera, but still need to be rendered for shadows for example.
     // Also, when running in the editor, the scene view cameras will also cause this function to be called.
-    void OnBecameInvisible()
+    IEnumerator OnBecameInvisible()
     {
-        ScreenWrap();
+        IsVisible = false;
+        yield return new WaitForSeconds(1f); // give object time to wrap before another call?
     }
 
 
     void OnBecameVisible()
     {
-        isWrappingX = false;
-        isWrappingY = false;
+        IsVisible = true;
     }
 
 
     void ScreenWrap()
     {
-        if(isWrappingX && isWrappingY)
-        {
-            return;
-        }
+        Vector3 viewportPosition = Vector3.zero;
+        viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
 
-        var cam = Camera.main;
-        var viewportPosition = cam.WorldToViewportPoint(transform.position);
-        var newPosition = transform.position;
+        Vector3 newPosition = transform.position;
 
-        if(!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
-        {
+        if(viewportPosition.x < 0 || viewportPosition.x > 1)
             newPosition.x = -newPosition.x;
 
-            isWrappingX = true;
-        }
-
-        if(!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
-        {
+        if(viewportPosition.y < 0 || viewportPosition.y > 1)
             newPosition.y = -newPosition.y;
 
-            isWrappingY = true;
-        }
-
+        // Deactivate the gameObject temporarily when we wrap/teleport.
+        // Prevents funky behaviour. (eg. particle systems based on distance emission)
+        //gameObject.SetActive(false);
         transform.position = newPosition;
-
+        //gameObject.SetActive(true);
     }
 }
