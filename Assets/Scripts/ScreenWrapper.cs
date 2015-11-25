24d9ef367454e07ece9fcf13ff0b4f7b92fa1250
antfarmar <markfarrugiamail@gmail.com>
2015-11-25 17:21:57 -0500

    Trying different algorithms.

--- Assets/Scripts/ScreenWrapper.cs
+++ Assets/Scripts/ScreenWrapper.cs
@@ -4,32 +4,51 @@
 // Hacky, see comments below. Affected by Scene view too.
 public class ScreenWrapper : MonoBehaviour
 {
+    bool isWrappingX = false;
+    bool isWrappingY = false;
 
     // Note that object is considered visible when it needs to be rendered in the scene.
     // It might not be actually visible by any camera, but still need to be rendered for shadows for example.
     // Also, when running in the editor, the scene view cameras will also cause this function to be called.
-    public void OnBecameInvisible()
+    void OnBecameInvisible()
     {
+        ScreenWrap();
+    }
 
-        if (gameObject.activeSelf) // && gameObject != null && Camera.main.isActiveAndEnabled)
+
+    void OnBecameVisible()
+    {
+        isWrappingX = false;
+        isWrappingY = false;
+    }
+
+
+    void ScreenWrap()
+    {
+        if(isWrappingX && isWrappingY)
         {
-            Vector3 viewportPosition = Vector3.zero;
-            if (Camera.main != null)
-                viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
-
-            Vector3 newPosition = transform.position;
-
-            if (viewportPosition.x < 0 || viewportPosition.x > 1)
-                newPosition.x = -newPosition.x;
-
-            if (viewportPosition.y < 0 || viewportPosition.y > 1)
-                newPosition.y = -newPosition.y;
-
-            // Deactivate the gameObject temporarily when we wrap/teleport.
-            // Prevents funky behaviour. (eg. particle systems based on distance emission)
-            //gameObject.SetActive(false);
-            transform.position = newPosition;
-            //gameObject.SetActive(true);
+            return;
         }
+
+        var cam = Camera.main;
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+        var newPosition = transform.position;
+
+        if(!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+
+            isWrappingX = true;
+        }
+
+        if(!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+
     }
 }
