48b988d39bc9fde3016f27de0e7aa2e34f9517ac
antfarmar <markfarrugiamail@gmail.com>
2015-12-08 19:55:53 -0500

    Redesign of the screenwrapping algorithm.
    - Removed all computations based on "visibility" in Unity API.
      - e.g. OnBecameInvisible
      - They're not consistent or precise enough.
    
    - Algorithm now uses bounds checking.
      - AABB bounds of an object's renderer. (could've also used Collider bounds)
      - checked against World bounds as a Rect
    
    - Objects also have a wrapping timeout to prevent constant wrapping lock.
      - If an object wraps in some direction, it shouldn't need to wrap again in the same direction right away!
      - This prevents objects from getting stuck on an edge by constantly wrapping back and forth.

--- Assets/Asteroids/Scripts/MicroBehaviours/ScreenWrapper.cs
+++ Assets/Asteroids/Scripts/MicroBehaviours/ScreenWrapper.cs
@@ -8,6 +8,121 @@ public class ScreenWrapper : MonoBehaviour
     [HideInInspector]
     public UnityEvent beforeWrap;
 
+    public float wrapTimeout = 0.5f;
+
+    Renderer objectRenderer;
+    Bounds objectBounds;
+    Rect worldRect;
+
+    Color sceneViewDisplayColor = new Color(1.0f, 0.0f, 1.0f, 0.5f);
+    bool debug = true;
+
+    bool allowedToWrapHorizontally = true;
+    bool allowedToWrapVertically = true;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (debug) DrawObjectBoundsInSceneView();
+
+        // Compute world rect boundds.
+        // Only need to do this if screen size changes? How to check?
+        Vector2 worldMin = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 0f));
+        Vector2 worldMax = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, 0f));
+        Debug.DrawLine(worldMin, worldMax, sceneViewDisplayColor);
+
+        //worldRect = new Rect(tl.x, tl.y, br.x * 2, tl.y * 2);
+        worldRect = Rect.MinMaxRect(worldMin.x, worldMin.y, worldMax.x, worldMax.y);
+        objectBounds = objectRenderer.bounds;
+
+        Vector3 newPosition = transform.position;
+
+        bool wrapRightToLeft = objectBounds.min.x > worldRect.xMax;
+        bool wrapLeftToRight = objectBounds.max.x < worldRect.xMin;
+        bool wrapTopToBottom = objectBounds.min.y > worldRect.yMax;
+        bool wrapBottomToTop = objectBounds.max.y < worldRect.yMin;
+
+        // Check horizontally out-of-view.
+
+        if (allowedToWrapHorizontally)
+        {
+            if (wrapRightToLeft)
+            {
+                newPosition.x = worldRect.xMin - objectBounds.extents.x;
+                allowedToWrapHorizontally = false;
+            }
+            else if (wrapLeftToRight)
+            {
+                newPosition.x = worldRect.xMax + objectBounds.extents.x;
+                allowedToWrapHorizontally = false;
+            }
+
+            if (!allowedToWrapHorizontally)
+                Invoke("AllowHorizontalWrap", wrapTimeout);
+        }
+
+        // Check vertically out-of-view.
+        if (allowedToWrapVertically)
+        {
+            if (wrapTopToBottom)
+            {
+                newPosition.y = worldRect.yMin - objectBounds.extents.y;
+                allowedToWrapVertically = false;
+            }
+            else if (wrapBottomToTop)
+            {
+                newPosition.y = worldRect.yMax + objectBounds.extents.y;
+                allowedToWrapVertically = false;
+            }
+
+            if (!allowedToWrapVertically)
+                Invoke("AllowVerticalWrap", wrapTimeout);
+        }
+
+        // if position actually changed...should go here.
+        if (!newPosition.Equals(transform.position))
+        {
+            beforeWrap.Invoke();
+            transform.position = newPosition;
+        }
+
+    }
+
+    void AllowHorizontalWrap() { allowedToWrapHorizontally = true; }
+    void AllowVerticalWrap() { allowedToWrapVertically = true; }
+
+    void DrawObjectBoundsInSceneView()
+    {
+        Vector3 lowerLeft = new Vector3(objectBounds.min.x, objectBounds.min.y, 0);
+        Vector3 upperLeft = new Vector3(objectBounds.min.x, objectBounds.max.y, 0);
+        Vector3 lowerRight = new Vector3(objectBounds.max.x, objectBounds.min.y, 0);
+        Vector3 upperRight = new Vector3(objectBounds.max.x, objectBounds.max.y, 0);
+
+        Debug.DrawLine(lowerLeft, upperLeft, sceneViewDisplayColor);
+        Debug.DrawLine(upperLeft, upperRight, sceneViewDisplayColor);
+        Debug.DrawLine(upperRight, lowerRight, sceneViewDisplayColor);
+        Debug.DrawLine(lowerRight, lowerLeft, sceneViewDisplayColor);
+    }
+
+}
+
+
+/* PREVIOUS IMPLEMENTATION
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScreenWrapper : MonoBehaviour
+{
+    [SerializeField]
+    [HideInInspector]
+    public UnityEvent beforeWrap;
+
     bool IsVisible = true;
 
     void Update()
@@ -57,3 +172,5 @@ public class ScreenWrapper : MonoBehaviour
         };
     }
 }
+
+*/
